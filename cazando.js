28ee9f1287ed43392024e702f0c6697ad4d98e7c
alilara564 <alilara701@gmail.com>
2026-04-12 23:50:27 -0900

    se pusieron las mecanicas faltantes

--- cazando.js
+++ cazando.js
@@ -15,8 +15,9 @@ let gatoY = 0;
 const ANCHOGATO = 50;
 const ALTURAGATO = 50;
 
-//puntos
+//complementos
 let puntos = 0;
+let tiempo;
  
 // Comida
 let comidaX = 50;
@@ -40,11 +41,14 @@ function graficarComida() {
 function iniciarJuego() {
     gatoX = (canvas.width / 2) - (ANCHOGATO / 2);
     gatoY = (canvas.height / 2) - (ALTURAGATO / 2);
-    comidaX = canvas.width - ANCHOCOMIDA;
-    comidaY = canvas.height - ALTURACOMIDA;
+    comidaX = 0;
+    comidaY = 0;
+    tiempo = 10;
+    puntos = 0;
     graficarGato();
     graficarComida();
-    
+    actualizarFotograma();
+    iniciarConteo();
 }
 
 function limpiarCanvas(){
@@ -85,20 +89,39 @@ function detectarColision(){
         gatoY < comidaY + ALTURACOMIDA &&
         gatoY + ALTURAGATO > comidaY
     ) {
-        alert("¡El gato comió!");
         sumarPuntos();
+        resetPositionComida();
     }
+}
+
+function resetPositionComida(){
+    comidaX = generarAleatorio(100,400)
+    comidaY = generarAleatorio(100,400)
 }
 
 function sumarPuntos(){
     puntos ++;
     mostrarEnSpan("lblPuntos",puntos);
 }
+
+function cuentaRegresiva(){
+    tiempo--;   
+    mostrarEnSpan("lblTiempo",parseInt(tiempo));
+    if (tiempo <= 0 && puntos < 6){
+        alert("Perdiste")
+        iniciarJuego();
+    }
+    else if (tiempo > 0 && puntos >= 6){
+        mostrarEnSpan("lbl tiempo", "Ganaste")
+    }
+}
+
+setInterval(cuentaRegresiva,1000)
  
 document.getElementById("botonArriba").onclick = () => moverArriba();
 document.getElementById("botonAbajo").onclick = () => moverAbajo();
 document.getElementById("botonIzquierda").onclick = () => moverIzquierda();
 document.getElementById("botonDerecha").onclick = () => moverDerecha();
  
-iniciarJuego();
-actualizarFotograma()
+actualizarFotograma();
+//setInterval(cuentaRegresiva,intervaloDeTiempo);
